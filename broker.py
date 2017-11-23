58e3347943bab422db15e4a9b922794e623306e9
Julien Chaffraix <julien.chaffraix@gmail.com>
2017-11-23 12:30:50 -0800

    Land a WIP result page
    The logic is mostly working and
    it's a matter of improving the UI
    
    and plugging a bit of math through.

--- broker.py
+++ broker.py
@@ -12,8 +12,14 @@ class NewPage(webapp2.RequestHandler):
     template = open('new.html').read()
     self.response.write(template)
 
+class ResultPage(webapp2.RequestHandler):
+  def get(self):
+    template = open('result.html').read()
+    self.response.write(template)
+
 app = webapp2.WSGIApplication([
     ('/', DashboardPage),
     ('/new', NewPage),
+    ('/result', ResultPage),
 # TODO: Should I switch to debug=False?
 ], debug=True)
